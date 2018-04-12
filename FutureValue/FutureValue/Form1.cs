0c937bc3315ee97f7c045508cef2134fa016c773
David J. Austin <djausti@outlook.com>
2018-04-12 18:47:28 -0500

    added CalculateFutureValue method

--- FutureValue/FutureValue/Form1.cs
+++ FutureValue/FutureValue/Form1.cs
@@ -25,15 +25,21 @@ namespace FutureValue
 
             int months = years * 12;
             decimal monthlyInterestRate = yearlyInterestRate / 12 / 100;
+            decimal futureValue = CalculateFutureValue(monthlyInvestment, months, monthlyInterestRate);
 
+            txtFutureValue.Text = futureValue.ToString("c");
+            txtMonthlyInvestment.Focus();
+        }
+
+        private static decimal CalculateFutureValue(decimal monthlyInvestment, int months, decimal monthlyInterestRate)
+        {
             decimal futureValue = 0m;
             for (int i = 0; i < months; i++)
             {
                 futureValue = (futureValue + monthlyInvestment) * (1 + monthlyInterestRate);
             }
 
-            txtFutureValue.Text = futureValue.ToString("c");
-            txtMonthlyInvestment.Focus();
+            return futureValue;
         }
 
         private void btnExit_Click(object sender, EventArgs e)
